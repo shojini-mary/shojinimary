bec1dc837ffabf6506b33dddc183e077cd22b4ff
shojini-mary <raytomita0506@icloud.com>
2023-11-12 00:05:05 +0900

    栃久保

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'shojinimary',
+  author: 'raychang',
+  headerTitle: '障子にメアリー',
+  description: '汎神論聞き飽きた',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
